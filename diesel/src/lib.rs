02340960e345f342e9c75ac245d2d9c066409fec
famouscat <onismaa@gmail.com>
2023-09-27 15:41:20 +0800

    chore: add allow to use internal_features in diesel_derives

--- diesel/src/lib.rs
+++ diesel/src/lib.rs
@@ -221,7 +221,7 @@
 //!
 //! - `with-deprecated`
 //! - `32-column-tables`
-
+#![allow(internal_features)]
 #![cfg_attr(feature = "nightly-error-messages", feature(rustc_attrs))]
 #![cfg_attr(feature = "unstable", feature(trait_alias))]
 #![cfg_attr(doc_cfg, feature(doc_cfg, doc_auto_cfg))]
